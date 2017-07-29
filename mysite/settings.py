662107568de7fc91dd1fb7a8ebedd5c3201d3952
Stephanie <stephaniecye@gmail.com>
2017-07-29 13:40:59 +1000

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@8^kd1g%e3pgx!yeubkpn-5@&9l=*52qzl5_km6h9849=ae7*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'scye.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'scye26.pythonanywhere.com']
 
 
 # Application definition
